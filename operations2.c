348cc8bfbd86dcc62cfc76a3ffa58f85567ce9e1
Ayman Msaoub <amsaoub@e1r8p8.1337.ma>
2023-01-05 14:33:57 +0100

    dhghjddgdgggfsggh

--- operations2.c
+++ operations2.c
@@ -6,7 +6,7 @@
 /*   By: amsaoub <amsaoub@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:44:49 by amsaoub           #+#    #+#             */
-/*   Updated: 2023/01/05 13:37:48 by amsaoub          ###   ########.fr       */
+/*   Updated: 2023/01/05 14:32:24 by amsaoub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void	rb(t_list **lst, int i)
 
 void	rr(t_list **lsta, t_list **lstb, int i)
 {
-	if (lst_size(lstb) < 2 || lst_size(lsta) < 2)
-		return ;
-	*lsta = (*lsta)->next;
-	*lstb = (*lstb)->next;
+	if (lst_size(lsta) > 1)
+		*lsta = (*lsta)->next;
+	if (lst_size(lstb) > 1)
+		*lstb = (*lstb)->next;
 	if (i == 1)
 		write(1, "rr\n", 3);
 }
